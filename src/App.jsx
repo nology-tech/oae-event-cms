63991a4b0294afb13ba24f45944334329dcd7b5d
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-12-13 16:57:18 +0000

    fixed the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import './App.scss';
+import "./App.scss";
 
-const = App => () {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
