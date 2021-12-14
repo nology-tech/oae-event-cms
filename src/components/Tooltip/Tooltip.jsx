0b6a41519e4de47ea024bc690b19748a05d791de
Roscoe Bull <roscoe-bull@live.co.uk>
2021-12-14 16:43:22 +0000

    added shadow and toggle function

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import infoIcon from "../../assets/images/info-icon.png";
 import blackCross from "../../assets/images/black-cross.png"
 import "./Tooltip.scss";
 
 const Tooltip = (props) => {
-   const { tipName, tipTitle, tipDesc } = props
+   const { tipName, tipTitle, tipDesc } = props;
+
+   const [showInfo, setShowInfo] = useState(false)
+
+   const toggleInfo = () => setShowInfo(!showInfo)
 
    return (
-      <div className={"tip tip__" + tipName}>
+      <div>
+         {!showInfo && <img className="tip__icon-info" src={infoIcon} alt="information icon" onClick={toggleInfo} />}
+         {showInfo && (<div className={"tip tip__" + tipName}>
 
-         <div className="tip__flex">
-            <h3 className='tip__flex-title'>{tipTitle}</h3>
-            <img className="tip__flex-icon" src={infoIcon} alt="information icon" />
-            <img className="tip__flex-icon" src={blackCross} alt="close window icon" />
-         </div>
+            <div className="tip__flex">
+               <h3 className='tip__title'>{tipTitle}</h3>
+               <img className="tip__icon-info" src={infoIcon} alt="information icon" />
+               <img onClick={toggleInfo} className="tip__icon-cross" src={blackCross} alt="close window icon" />
+            </div>
 
-         <p className='tip__desc'>{tipDesc}</p>
+            <p className='tip__desc'>{tipDesc}</p>
+         </div>)}
       </div>
    )
 }
