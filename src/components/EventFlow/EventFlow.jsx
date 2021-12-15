49ca1f9b4be7bf51b60efc5d386710cdd1b595f4
4naans <mylespillay@yahoo.co.uk>
2021-12-15 14:52:22 +0000

    styling updated

--- src/components/EventFlow/EventFlow.jsx
+++ src/components/EventFlow/EventFlow.jsx
@@ -1,4 +1,4 @@
-import React from"react";
+import React, { useState } from"react";
 import "./EventFlow.scss";
 import {ReactComponent as InfoIcon} from "../../assets/images/info-icon.svg";
 import {ReactComponent as ScheduleIcon} from "../../assets/images/music-list-circle.svg";
@@ -9,36 +9,59 @@ import {ReactComponent as TickIcon} from "../../assets/images/tick-circle.svg";
 
 
 const EventFlow = () => {
-    
+
+const [ completed, setCompleted ] = useState(true);
+//const handleClick = () => {
+    //this.setCompleted({ completed: true})
+
+const handleClick = () => {
+    setCompleted(!completed);
+};
+
+
 //3 columns six rows, text 1 to 3, icons 3-4
 return (
-    <div className="event-flow_container">
-     
-        <h3 className="event-flow__text" >Event Information</h3>
-        <p className="event-flow__text">Name, Date,  Time, Description etc</p>
-        <InfoIcon className="event-flow__icon" id="infoIcon"/>
-
-
-        <h3 className="event-flow__text1">Add Schedule</h3>
-        <p className="event-flow__text1">Order of Play, Lyrics, Speeches etc</p>
-        <ScheduleIcon className="event-flow__icon1" id="scheduleIcon"/>
-
-
-        <h3 className="event-flow__text2">Add Orchestra</h3>
-        <p className="event-flow__text2">Instruments, Choir, Speakers</p>
-        <OrchestraIcon className="event-flow__icon2"/>
-
-
-        <h3 className="event-flow__text3">Choose Theme</h3>
-        <p className="event-flow__text3">Colors, Fonts etc</p>
-        <ThemeIcon className="event-flow__icon3"/>
-
-
-        <h3 className="event-flow_text4">Review</h3>
-        <p className="event-flow_text4">Colors, Fonts etc</p>
-        <TickIcon className="event-flow__icon4"/>
+    <div className="event-flow__container">
+        <h1 className="event-flow__title"> create event</h1>
+        <h3 className="event-flow__header1" >Event Information</h3>
+        <p className="event-flow__text1">Name, Date,  Time, Description etc</p>
         
+            {useState.completed ? <InfoIcon className="event-flow__icon1" id="icon" onClick={handleClick} /> 
+            : <InfoIcon className="event-flow__icon1" id="icon" onClick={handleClick} /> }
+      
+
+        <h3 className="event-flow__header2">Add Schedule</h3>
+        <p className="event-flow__text2">Order of Play, Lyrics, Speeches etc</p>
+        
+            {useState.completed ? <ScheduleIcon className="event-flow__icon2" id="icon" onClick={handleClick} /> 
+            : <ScheduleIcon className="event-flow__icon2" id="icon" onClick={handleClick} /> }
+   
+
+
+        <h3 className="event-flow__header3">Add Orchestra</h3>
+        <p className="event-flow__text3">Instruments, Choir, Speakers</p>
+
+            {useState.completed ? <OrchestraIcon className="event-flow__icon3" id="icon" onClick={handleClick} /> 
+            : <OrchestraIcon className="event-flow__icon3" id="icon" onClick={handleClick} /> }
+      
+
+
+        <h3 className="event-flow__header4">Choose Theme</h3>
+        <p className="event-flow__text4">Colors, Fonts etc</p>
+     
+            {useState.completed ? <ThemeIcon className="event-flow__icon4" id="icon" onClick={handleClick} /> 
+            : <ThemeIcon className="event-flow__icon4" id="icon" onClick={handleClick} /> }
+  
+    
+
+        <h3 className="event-flow__header5">Review</h3>
+        <p className="event-flow__text5">Colors, Fonts etc</p>
+     
+            {useState.completed ? <TickIcon className="event-flow__icon5" id="icon" onClick={handleClick} /> 
+            : <TickIcon className="event-flow__icon5" id="icon" onClick={handleClick} /> }
+   
     </div>
     )
-}
- export default EventFlow 
+    }
+;
+ export default EventFlow ;
