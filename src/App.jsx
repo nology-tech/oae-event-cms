dff578616a17a14ad4c13558f9a3321ce703c9c1
Roscoe Bull <roscoe-bull@live.co.uk>
2021-12-16 17:15:28 +0000

    pulled from main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.scss";
+
 import Nav from "./containers/Nav/Nav";
 import EventsList from "./containers/EventsList/EventsList";
 import CreateEvent from "./containers/CreateEvent/CreateEvent";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header/Header";
 
 const App = () => {
   return <div className="App">
+    <Header />
     <Router>
       <Nav />
       <Routes>
