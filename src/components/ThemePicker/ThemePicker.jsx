6533d9ebc1c6083804398543c0c5763c66fc683e
Khemi <khemi@Khemis-Laptop.local>
2021-12-16 16:41:20 +0000

    setup-create-theme-picker-page

--- src/components/ThemePicker/ThemePicker.jsx
+++ src/components/ThemePicker/ThemePicker.jsx
@@ -1,33 +1,75 @@
 import React from "react";
 import Button from "../Button/Button";
 import "./ThemePicker.scss";
-
+import arrowDown from "../../assets/images/arrow-down.svg";
 
 const ThemePicker = () => {
   return (
-    <form className="themepicker">
-      <p className="themepicker__subheading">Step 04</p>
-      <h2 className="themepicker__heading">Choose Theme</h2>
-      <div className="themepicker__dropdown">
-          <label for="fonttype">Font Type</label>
-          <select id="fonttype" name="fonttype">
+    <form className="themepicker form">
+      <div className="themepicker form__title">
+        <p className="themepicker form__title__step">Step 04</p>
+        <h2 className="themepicker form__title__main">Choose Theme</h2>
+      </div>
+
+      <div className="themepicker__dropdown form__section">
+        <div className="themepicker form__section">
+          <label className="themepicker form__label" for="fonttype">
+            Font Type
+          </label>
+          <select
+            className="themepicker__dropdown form__selectBox"
+            id="fonttype"
+            name="fonttype"
+          >
+            <option value="" disabled selected>
+              Please select from one of the following...
+            </option>
             <option value="Modern">Modern</option>
             <option value="Classic">Classic</option>
           </select>
-          <div className="themepicker__theme">
-            <label for= "theme">Theme Color</label>
-            <input for ="theme" type="color" />
+        </div>
+
+        <div className="themepicker form__section">
+          <label className="themepicker__theme form__label" for="theme">
+            Theme Color
+          </label>
+          <div className="themepicker__accent form__section-color">
+            <input
+              className="themepicker__accent form__input"
+              for="theme"
+              type="color"
+            />
+            <img
+              className="themepicker__accent form__section-img"
+              src={arrowDown}
+              alt="down arrow"
+            />
           </div>
-          <div className="themepicker__accent">
-            <label for= "accent">Accent Color</label>
-            <input for ="accent" type="color" />
+        </div>
+
+        <div className="themepicker form__section">
+          <label className="themepicker__accent form__label" for="accent">
+            Accent Color
+          </label>
+          <div className="themepicker__accent form__section-color">
+            <input
+              className="themepicker__accent form__input"
+              for="accent"
+              type="color"
+            />
+            <img
+              className="themepicker__accent form__section-img"
+              src={arrowDown}
+              alt="down arrow"
+            />
           </div>
+        </div>
       </div>
-      <div className="buttons">
+      <div className="themepicker form__buttons">
         {/* BACK button */}
-        <Button buttonText="Back" buttonType="secondary"/>
+        <Button buttonText="Back" buttonType="secondary" />
         {/* NEXT button */}
-        <Button buttonText="Next" buttonType="primary"/>
+        <Button buttonText="Next" buttonType="primary" />
       </div>
     </form>
   );
