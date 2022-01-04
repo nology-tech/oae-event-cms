a5387b6e063ceae4361531b786b183c105ae8c9f
4naans <mylespillay@yahoo.co.uk>
2022-01-04 14:58:11 +0000

    major changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./containers/Header/Header";
 import EventsList from "./containers/EventsList/EventsList";
 import CreateEvent from "./containers/CreateEvent/CreateEvent";
+import PageOne from "./components/Event-Info-Form/Page2/Page2";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/orchestra" />
         </Routes>
       </Router>
+      <PageOne/>
       </div>
   );
 };
--- src/components/Event-Info-Form/Page2/Page2.jsx
+++ src/components/Event-Info-Form/Page2/Page2.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Page2.scss";
 import Button from "../../Button/Button";
+
 const PageTwo = () => {
 return (
      <div className="create-event-info-page" >
@@ -12,16 +13,16 @@ return (
 
             <div className="form" id="info-form">
                 <h4 className="form__title"> Event Heading </h4>
-                <input className="form__input" type="text" placeholder="Is This Is Your First Bach, the Universe And Everyt...">
+                <input className="form__input" type="text" placeholder="If This Is Your First Bach, the Universe And Everyt...">
                 </input>
                 <h4 className="form__title"> Add Quote / Note (optional) </h4>
                 <input className="form__input" type="text" placeholder="A note from our principal Keyboard Stephen.."/>
                 <h4 className="form__title"> Add Quote Caption (optional) </h4>
                 <input type="text" className="form__input"  />
             </div>
-            <div className="form" id="description">
+            <div className="form" >
                 <h4 className="form__title"> Event Description </h4>
-                <input type="text" className="form__input" rows="60" placeholder="We like to think of the series as a community, similar to the one Bach enjoyed in Leipzig where he produced contatas at an extraordinary rate, providing innovative music for the weekly services at the church where he worked from 1723 until his death in 1750. At that time, the congregation was as open to science and new ideas as it was united by faith. It is this spirit that we cae together for a thought provoking contata and a talk..."/>
+                <input className="form__input" id="description" width="100" rows="80" placeholder="We like to think of the series as a community, similar to the one Bach enjoyed in Leipzig where he produced contatas at an extraordinary rate, providing innovative music for the weekly services at the church where he worked from 1723 until his death in 1750. At that time, the congregation was as open to science and new ideas as it was united by faith. It is this spirit that we cae together for a thought provoking contata and a talk..."/>
             </div>
             <div>
                 <Button className="secondary">Back</Button>
