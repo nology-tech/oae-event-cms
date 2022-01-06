9ada8a69f17af5856be127eb6cfc526f66aaa1a2
Ben Augood <ben_augood@outlook.com>
2022-01-06 17:12:13 +0000

    Fixed BEM naming

--- src/components/ThemePicker/ThemePicker.jsx
+++ src/components/ThemePicker/ThemePicker.jsx
@@ -6,14 +6,14 @@ import arrowDown from "../../assets/images/arrow-down.svg";
 const ThemePicker = () => {
   return (
     <form className="themepicker form">
-      <div className="themepicker form__title">
-        <p className="themepicker form__title-step">Step 04</p>
-        <h2 className="themepicker form__title-main">Choose Theme</h2>
+      <div className="themepicker__title form__title">
+        <p className="form__title-step">Step 04</p>
+        <h2 className="form__title-main">Choose Theme</h2>
       </div>
 
       <div className="themepicker__dropdown form__section">
-        <div className="themepicker form__section">
-          <label className="themepicker form__label" for="fonttype">
+        <div className="themepicker__section form__section">
+          <label className="themepicker__label form__label" for="fonttype">
             Font Type
           </label>
           <select
@@ -29,35 +29,35 @@ const ThemePicker = () => {
           </select>
         </div>
 
-        <div className="themepicker form__section">
+        <div className="themepicker__section form__section">
           <label className="themepicker__theme form__label" for="theme">
             Theme Color
           </label>
-          <div className="themepicker__accent form__section-color">
-            <input className="themepicker__input" for="theme" type="color" />
+          <div className="themepicker__section-color form__section-color">
+            <input className="themepicker__input-color" for="theme" type="color" />
             <img
-              className="themepicker__accent form__section-img"
+              className="themepicker__section-img form__section-img"
               src={arrowDown}
               alt="down arrow"
             />
           </div>
         </div>
 
-        <div className="themepicker form__section">
+        <div className="themepicker__section form__section">
           <label className="themepicker__accent form__label" for="accent">
             Accent Color
           </label>
-          <div className="themepicker__accent form__section-color">
-            <input className="themepicker__input" for="accent" type="color" />
+          <div className="themepicker__section-color form__section-color">
+            <input className="themepicker__input-color" for="accent" type="color" />
             <img
-              className="themepicker__accent form__section-img"
+              className="themepicker__section-img form__section-img"
               src={arrowDown}
               alt="down arrow"
             />
           </div>
         </div>
       </div>
-      <div className="themepicker form__buttons">
+      <div className="themepicker__buttons form__buttons">
         {/* BACK button */}
         <Button buttonText="Back" buttonType="secondary" />
         {/* NEXT button */}
