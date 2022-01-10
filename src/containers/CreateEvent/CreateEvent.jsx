4bb80390db8ddad287132dd75fce05f24a5221a5
4naans <mylespillay@yahoo.co.uk>
2022-01-10 16:56:16 +0000

    cleared data from event export

--- src/containers/CreateEvent/CreateEvent.jsx
+++ src/containers/CreateEvent/CreateEvent.jsx
@@ -6,8 +6,49 @@ import ThemePicker from "../../components/ThemePicker/ThemePicker";
 import PageOne from '../../components/EventInfoForm/PageOne/PageOne';
 import PageTwo from '../../components/EventInfoForm/PageTwo/PageTwo';
 
-const CreateEvent = () => {
+const CreateEvent = () => {""
+     let event = {
+            name: "",
+            series: "",
+            time: "",
+            date: "",
+            location: "",
+            imageSrc: "",
+            imageAlt: "",
+            featuredEvent: true,
+            intro: {
+                heading:
+                    "",
+                content:
+                    "",
+                quote: "",
+                quoteCaption: null,
+            },
+            schedule: [
+                {
+                    name: "",
+                    title: null,
+                    author: null,
+                    description: null,
+                    alternateDescription: null,
+                },
+                {
+                    name: "",
+                    title: "",
+                    author: "",
+                    description: "",
+                    alternateDescription: "", // This is used for any lyric translations. It will create a tab in the component that the end-user can switch between.
+                },
+            ],
+            orchestra: [{}],
+            theme: {
+                templateTheme: "", // Modern or Classic. Modern uses a sans-serif font. (Poppins and PT Sans) Classic uses a serif (Playfair Display and Lato).
+                primaryColor: "", // Represents the banner color on the website. Usually takes the dominant color present in the image poster.
+                accentColor: "", // Accent Colour is used to style the call-to-action buttons.
 
+            },
+        };
+    
     const [step, setStep] = useState(0);
 
     const incrementStep = () => {
