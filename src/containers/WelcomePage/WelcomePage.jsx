e3f0b312b40880b3d2f2de3c5ff855acbfe4966b
Khemi <khemi@Khemis-Laptop.local>
2022-01-10 17:08:32 +0000

    added reusable login template component

--- src/containers/WelcomePage/WelcomePage.jsx
+++ src/containers/WelcomePage/WelcomePage.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import "./WelcomePage.scss";
 import Button from "../../components/Button/Button";
-import logo from "../../assets/images/oae-logo.svg";
-import background from "../../assets//images/welcome-background.svg";
-import greyRectangle from "../../assets//images/grey-rectangle.svg";
-import eventListPreview from "../../assets//images/eventlist-preview.svg";
-import orangeRectangle from "../../assets//images/orange-rectangle.svg";
-import circle from "../../assets//images/ellipse.svg";
+import { LoginTemplate } from "../../components/LoginTemplate/LoginTemplate";
 
 const WelcomePage = () => {
-  return (
-    <div className="welcomePage">
-      <a href="https://oae.co.uk/" target="_blank" rel="noreferrer">
-        <img className="welcomePage__logo" src={logo} alt="logo" />
-      </a>
+  return (<>
+    
       <div className="welcome">
         <h1 className="welcome__header">
           Welcome back to the{" "}
@@ -46,37 +38,8 @@ const WelcomePage = () => {
           <Button buttonText="Login" buttonType="primary"></Button>
         </div>
       </div>
-      <img
-        className="welcomePage__image"
-        src={background}
-        alt="orange background"
-      />
-      <img
-        className="welcomePage__circle-big"
-        src={circle}
-        alt="orange circle"
-      />
-      <img
-        className="welcomePage__circle-small"
-        src={circle}
-        alt="orange circle"
-      />
-      <img
-        className="welcomePage__rectangle-grey"
-        src={greyRectangle}
-        alt="grey rectangle"
-      />
-      <img
-        className="welcomePage__rectangle-orange"
-        src={orangeRectangle}
-        alt="eventList preview"
-      />
-      <img
-        className="welcomePage__preview"
-        src={eventListPreview}
-        alt="eventList preview"
-      />
-    </div>
+      <LoginTemplate />
+      </>
   );
 };
 
