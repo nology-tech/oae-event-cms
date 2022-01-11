da3aceb9df6ca811f264e5686dad9c98b0b80d80
Khemi <khemi@Khemis-Laptop.local>
2022-01-11 15:40:59 +0000

    added props to forgot password

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import Button from "../Button/Button";
 import "./ForgotPassword.scss";
 
-const ForgotPassword = () => {
+const ForgotPassword = (props) => {
+
+    const handleNext = (event) => {
+        event.preventDefault();
+        props.handleNext();
+      }
+
     return (
         <div className="forgot">
             <div className="left-container">
-                <form>
+                <form onSubmit={handleNext}>
                     <h1 className="left-container__header">Forgotten Your <span className="left-container__header--orange"> Password.</span> </h1>
                     <p className="left-container__subtitle">Please enter your email address below to reset your password.</p>
                     <label className="left-container__label">Email Address</label>
