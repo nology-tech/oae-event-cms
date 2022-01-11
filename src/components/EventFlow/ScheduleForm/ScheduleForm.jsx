963cee2076ce001791b1c57f65b82a568d0e7936
Roscoe Bull <roscoe-bull@live.co.uk>
2022-01-11 15:20:11 +0000

    add validation to pageTwo and schedule form

--- src/components/EventFlow/ScheduleForm/ScheduleForm.jsx
+++ src/components/EventFlow/ScheduleForm/ScheduleForm.jsx
@@ -2,50 +2,74 @@ import React from "react";
 import Button from "../../Button/Button";
 import "./ScheduleForm.scss";
 import TextareaAutosize from 'react-textarea-autosize';
+import { useForm } from "react-hook-form";
 
 const ScheduleForm = (props) => {
 
-  const handleSubmit = (event) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data, event) => {
     event.preventDefault();
     props.handleSubmit();
-  }
-
+    console.log(data);
+  };
   return (
     <>
-    <form className ="schedule-form form" onSubmit={handleSubmit}>
-     <div className="form__title">
+      <form className="schedule-form form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="form__title">
           <h3 className="form__title-step">Step 02</h3>
           <h2 className="form__title-main">Add Schedule</h2>
-      </div>
+        </div>
 
-      <div className="schedule-form__name form__section">  
-        <label for="name" className="form__label">Name</label>
-        <input type="text" id="name" name="name" className="form__input" required></input>
-      </div>
-      <div className="schedule-form__title form__section">
-        <label for="title" className="form__label">Title (optional)</label>
-        <input type="text" id="title" name="title" className="form__input"></input>
-      </div>
-      <div className="schedule-form__author form__section">
-        <label for="author" className="form__label">Author (optional)</label>
-        <input type="text" id="author" name="author" className="form__input"></input>
-      </div>
-      <div className="schedule-form__description form__section">
-        <label for="description" className="form__label">Description</label>
-        <TextareaAutosize name="description" id="description" className="form__input" required />
-      </div>  
-      <div className = "schedule-form__alternate-description form__section">
-        <label for="alternate-description" className="form__label">
+        <div className="schedule-form__name form__section">
+          <label for="name" className="form__label">Name</label>
+          <input type="text" id="name" name="name" className="form__input" {...register("name", {
+            required: "This field is required",
+            minLength: {
+              value: 2,
+              message: "The name needs to contain at least 2 characters",
+            },
+          })}
+          />
+          {errors.name && <p>{errors.name.message}</p>}
+
+        </div>
+        <div className="schedule-form__title form__section">
+          <label for="title" className="form__label">Title (optional)</label>
+          <input type="text" id="title" name="title" className="form__input"></input>
+        </div>
+        <div className="schedule-form__author form__section">
+          <label for="author" className="form__label">Author (optional)</label>
+          <input type="text" id="author" name="author" className="form__input"></input>
+        </div>
+        <div className="schedule-form__description form__section">
+          <label for="scheduleDescription" className="form__label">Description</label>
+          <TextareaAutosize name="scheduleDescription" id="scheduleDescription" className="form__input"  {...register("scheduleDescription", {
+            required: "This field is required",
+            minLength: {
+              value: 2,
+              message: "The description needs to contain at least 2 characters",
+            },
+          })}
+          />
+          {errors.scheduleDescription && <p>{errors.scheduleDescription.message}</p>}
+        </div>
+        <div className="schedule-form__alternate-description form__section">
+          <label for="alternate-description" className="form__label">
             Alternate Description (optional)
-        </label>
-        <TextareaAutosize name="alternate-description" id="alternate-description" className="form__input" placeholder="E.g.Lyric Translations" cacheMeasurements
-        />
-      </div>
-      <div className="schedule-form__buttons form__buttons"> 
-        <Button buttonType="tertiary" buttonText="Add" className="form__button-add"/>
-        <Button buttonType="primary" buttonText="Next" className="form__button-next" type="submit"/>
-      </div>
-    </form>
+          </label>
+          <TextareaAutosize name="alternate-description" id="alternate-description" className="form__input" placeholder="E.g.Lyric Translations" cacheMeasurements
+          />
+        </div>
+        <div className="schedule-form__buttons form__buttons">
+          <Button buttonType="tertiary" buttonText="Add" className="form__button-add" />
+          <Button buttonType="primary" buttonText="Next" className="form__button-next" type="submit" />
+        </div>
+      </form>
     </>
   );
 };
--- src/components/EventInfoForm/PageTwo/PageTwo.jsx
+++ src/components/EventInfoForm/PageTwo/PageTwo.jsx
@@ -1,43 +1,77 @@
 import React from "react";
 import "./PageTwo.scss";
 import Button from "../../Button/Button";
+import { useForm } from "react-hook-form";
 
 const PageTwo = (props) => {
 
-    const handleSubmit = (event) => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = (data, event) => {
         event.preventDefault();
         props.handleSubmit();
-    }
+        console.log(data);
+    };
 
     return (
-     <form className="create-event-info-page" onSubmit={handleSubmit}>
-        <div className="event-info-header">
-            <h3 className="create-event-info__step">Step 02</h3>
-            <h1 className="create-event-info__title">Event Information (2/2)</h1>
-        </div>
-        <div className="event-info-form">
+        <form className="create-event-info-page" onSubmit={handleSubmit(onSubmit)}>
+            <div className="event-info-header">
+                <h3 className="create-event-info__step">Step 02</h3>
+                <h1 className="create-event-info__title">Event Information (2/2)</h1>
+            </div>
+            <div className="event-info-form">
 
-            <div className="form" id="info-form">
-                <h4 className="form__title"> Event Heading </h4>
-                <input className="form__input" type="text" placeholder="If This Is Your First Bach, the Universe And Everyt...">
-                </input>
-                <h4 className="form__title"> Add Quote / Note (optional) </h4>
-                <input className="form__input" type="text" placeholder="A note from our principal Keyboard Stephen.."/>
-                <h4 className="form__title"> Add Quote Caption (optional) </h4>
-                <input type="text" className="form__input"  />
-            </div>
-            <div className="description form" >
-                <h4 className="description__title form__title"> Event Description </h4>
-                <input className=" description__input form__input" id="description" width="100" rows="80" placeholder="We like to think of the series as a community, similar to the one Bach enjoyed in Leipzig where he produced contatas at an extraordinary rate, providing innovative music for the weekly services at the church where he worked from 1723 until his death in 1750. At that time, the congregation was as open to science and new ideas as it was united by faith. It is this spirit that we cae together for a thought provoking contata and a talk..."/>
-                <div className="description__button form__button">
-                    <Button buttonType="secondary" buttonText="Back"className="secondary"/>
-                    <Button buttonType="primary"
-                buttonText="Next"className="primary"/>
+                <div className="form" id="info-form">
+                    <h4 className="form__title"> Event Heading </h4>
+                    <input
+                        className="form__input"
+                        type="eventHeading"
+                        name="eventHeading"
+                        id="eventHeading"
+                        placeholder="If This Is Your First Bach, the Universe And Everyt..."
+                        {...register("eventHeading", {
+                            required: "This field is required",
+                            minLength: {
+                                value: 2,
+                                message: "The event heading needs to contain at least 2 characters",
+                            },
+                        })} />
+                    {errors.eventHeading && <p>{errors.eventHeading.message}</p>}
+                    <h4 className="form__title"> Add Quote / Note (optional) </h4>
+                    <input className="form__input" type="text" placeholder="A note from our principal Keyboard Stephen.." />
+                    <h4 className="form__title"> Add Quote Caption (optional) </h4>
+                    <input type="text" className="form__input" />
                 </div>
+                <div className="description form" >
+                    <h4 className="description__title form__title"> Event Description </h4>
+                    <input
+                        className=" description__input form__input"
+                        id="description"
+                        name="description"
+                        width="100"
+                        rows="80"
+                        placeholder="We like to think of the series as a community, similar to the one Bach enjoyed in Leipzig where he produced contatas at an extraordinary rate, providing innovative music for the weekly services at the church where he worked from 1723 until his death in 1750. At that time, the congregation was as open to science and new ideas as it was united by faith. It is this spirit that we cae together for a thought provoking contata and a talk..."
+                        {...register("description", {
+                            required: "This field is required",
+                            minLength: {
+                                value: 2,
+                                message: "The description needs to contain at least 2 characters",
+                            },
+                        })} />
+                    {errors.description && <p>{errors.description.message}</p>}
+                    <div className="description__button form__button">
+                        <Button buttonType="secondary" buttonText="Back" className="secondary" />
+                        <Button buttonType="primary"
+                            buttonText="Next" className="primary" />
+                    </div>
+                </div>
+
             </div>
-            
-        </div>
-     </form>
-        )
-    }
+        </form>
+    )
+}
 export default PageTwo
