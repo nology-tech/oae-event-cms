a56a8e5a91cd8ce5ff46d9f41dba259b4447558f
Ami-007 <amivfletcher@gmail.com>
2022-01-13 00:47:53 +0000

    Fixed Event Flow styling

--- src/components/EventFlow/EventFlow.jsx
+++ src/components/EventFlow/EventFlow.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from"react";
 import "./EventFlow.scss";
-import {ReactComponent as InfoIcon} from "../../assets/images/info-icon.svg";
 import {ReactComponent as ScheduleIcon} from "../../assets/images/music-list-circle.svg";
 import {ReactComponent as OrchestraIcon} from "../../assets/images/single-note-circle.svg";
 import {ReactComponent as ThemeIcon} from "../../assets/images/paintbrush-circle.svg";
-import {ReactComponent as TickIcon} from "../../assets/images/tick-circle.svg";
+import {ReactComponent as TickIcon} from "../../assets/images/tick-in-circle.svg";
+import {ReactComponent as InfoIcon} from "../../assets/images/info-circle.svg";
 
 const EventFlow = () => {
 const [ completed, setCompleted ] = useState(true);
