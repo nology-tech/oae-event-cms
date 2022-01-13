3ec4690626cfe98fb580e56e561369610f74b5f4
Roscoe Bull <roscoe-bull@live.co.uk>
2022-01-13 15:24:00 +0000

    added logout button image and styled appropriately

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Header.scss";
 import logo from "../../assets/images/oae-logo.svg";
+import { ReactComponent as Logout } from "../../assets/images/logout-icon.svg";
 
 const Header = (props) => {
+
   return (
     <div className="header">
-      <a href="https://oae.co.uk/" target="_blank" rel= "noreferrer"><img className="header__logo" src={logo} alt="logo"/></a>
+      <a href="https://oae.co.uk/" target="_blank" rel="noreferrer"><img className="header__logo" src={logo} alt="logo" /></a>
       <h2 className="header__title">OAE Digital Programme Editor</h2>
+      <Logout className="header__button-logout" />
     </div>
   );
 };
