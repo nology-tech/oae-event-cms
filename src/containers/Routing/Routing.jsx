cbc2f14c98f6175f62e1c5f6ba33c53e27ec3f34
Roscoe Bull <roscoe-bull@live.co.uk>
2022-01-18 14:32:08 +0000

    styled and routed error page

--- src/containers/Routing/Routing.jsx
+++ src/containers/Routing/Routing.jsx
@@ -6,17 +6,17 @@ import Login from "../Login/Login.jsx";
 import ErrorPage from "../ErrorPage/ErrorPage.jsx";
 
 const Routing = () => {
- return <>
-    <Routes>
-      <Route path="/create/event" element={<CreateEvent/>}/>
-      <Route path="/" element={<EventsList />} />
-      <Route path="*" element={<ErrorPage />} />
-      
-       {/* Temporary */}
-       <Route path="/login" element={<Login />} />
-       <Route path="/reset" element={<ResetPassword />} />
-    </Routes>
- </>
+   return <>
+      <Routes>
+         <Route path="/create/event" element={<CreateEvent />} />
+         <Route path="/" element={<EventsList />} />
+         <Route path="*" element={<ErrorPage />} />
+
+         {/* Temporary */}
+         <Route path="/login" element={<Login />} />
+         <Route path="/reset" element={<ResetPassword />} />
+      </Routes>
+   </>
 }
 
 export default Routing;
