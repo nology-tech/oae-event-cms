2b83153eea882f1a5e82e84c23e456d9777e1299
Andy Evans <andy@nology.io>
2022-01-20 15:45:11 +0000

    Added logout button

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./Header.scss";
 import logo from "../../assets/images/oae-logo.svg";
 import { ReactComponent as Logout } from "../../assets/images/logout-icon.svg";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
+
   return (
     <div className="header">
       <a href="https://oae.co.uk/" target="_blank" rel= "noreferrer">
         <img className="header__logo" src={logo} alt="logo"/></a>
         <h2 className="header__title">OAE Digital Programme Editor</h2>
-        <Logout />
+        <Link className="header__button-logout" to="/">
+          <Logout />
+        </Link>
     </div>
   );
 };
