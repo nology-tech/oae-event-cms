4d58159f1c1e7576c5229b31b355ad304ad8e634
Andy Evans <andy@nology.io>
2022-01-20 15:40:30 +0000

    temp

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.scss";
 import logo from "../../assets/images/oae-logo.svg";
+import { ReactComponent as Logout } from "../../assets/images/logout-icon.svg";
 
 const Header = (props) => {
   return (
@@ -8,6 +9,7 @@ const Header = (props) => {
       <a href="https://oae.co.uk/" target="_blank" rel= "noreferrer">
         <img className="header__logo" src={logo} alt="logo"/></a>
         <h2 className="header__title">OAE Digital Programme Editor</h2>
+        <Logout />
     </div>
   );
 };
